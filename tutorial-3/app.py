98bda5f9bece6c30c9bc4c8a06a4692da3c33acd
Sabrina Matthews <sabrina.matthews@mymona.uwi.edu>
2026-09-16 12:25:58 -0700

    task 0: setup

---
+++ tutorial-3/app.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI, HTTPException
+
+app = FastAPI()
+
+readings = [
+    {"name": "front-door", "room": "hall",    "temp": 27.4, "online": True},
+    {"name": "hall-lamp",  "room": "hall",    "temp": 26.1, "online": True},
+    {"name": "attic",      "room": "attic",   "temp": 31.9, "online": True},
+    {"name": "fridge",     "room": "kitchen", "temp": 4.2,  "online": False},
+    {"name": "patio",      "room": "outside", "temp": 29.8, "online": True},
+]
+
+def average_temp(devices):
+    return sum(device['temp'] for device in devices) / len(devices)
+
+print(average_temp(readings))
+
+def hottest_device(devices):
+    return max(devices, key=lambda device: device['temp'])
